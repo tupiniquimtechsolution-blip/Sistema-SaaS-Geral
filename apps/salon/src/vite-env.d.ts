182111a64c1b4371ccb711d18b1b33855df49a58
Rodrigo Tartaruga <tupiniquim.techsolution@gmail.com>
2026-09-24 15:16:36 -0300

    chore(salon): type live tenant slug env

--- apps/salon/src/vite-env.d.ts
+++ apps/salon/src/vite-env.d.ts
@@ -4,7 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_DEMO_MODE?: string;
   readonly VITE_SUPABASE_URL?: string;
   readonly VITE_SUPABASE_PUBLISHABLE_KEY?: string;
-  readonly VITE_TENANT_ID?: string;
+  readonly VITE_TENANT_SLUG?: string;
 }
 
 interface ImportMeta {
